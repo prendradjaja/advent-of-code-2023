444487440fb1ceb647e97891dbeb3442b64dde39
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2023-12-09 02:14:09 -0600

    Day 9

---
+++ 09--mirage-maintenance/a.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+'''
+Usage:
+    ./a.py PATH_TO_INPUT_FILE
+    ./b.py PATH_TO_INPUT_FILE
+'''
+
+import sys
+
+
+def main():
+    xss = [
+        [int(x) for x in line.split()]
+        for line in open(sys.argv[1]).read().splitlines()
+    ]
+    answer = sum(extrapolate(xs) for xs in xss)
+    print(answer)
+
+
+def diffs(xs):
+    return [
+        b - a
+        for a, b
+        in zip(xs, xs[1:])
+    ]
+
+
+def extrapolate(xs):
+    if all(x == 0 for x in xs):
+        return 0
+    next_diff = extrapolate(diffs(xs))
+    return xs[-1] + next_diff
+
+
+if __name__ == '__main__':
+    main()
