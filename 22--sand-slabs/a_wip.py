b89dbe70c1a5d7aa3a57e67d4026fd9b526235d2
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2023-12-22 13:11:37 -0600

    Day 22 part 1: WIP

---
+++ 22--sand-slabs/a_wip.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+'''
+Usage:
+    ./a.py PATH_TO_INPUT_FILE
+    ./b.py PATH_TO_INPUT_FILE
+'''
+
+import sys
+from dataclasses import dataclass
+import collections
+import ast
+
+
+X, Y, Z = 0, 1, 2
+DOWN = (0, 0, -1)
+
+
+def main():
+    '''
+    >>> import unittest.mock
+    >>> with unittest.mock.patch('sys.argv', [None, './ex']):
+    ...    main()
+    5
+    '''
+    bricks = []
+    for line in open(sys.argv[1]).read().splitlines():
+        start, end = line.split('~')
+        start = ast.literal_eval(start)
+        end = ast.literal_eval(end)
+        bricks.append(Brick(start, end))
+
+    fall(bricks)
+
+    answer = 0
+    for brick in bricks:
+        other_bricks = [b for b in bricks if b != brick]
+        assert len(other_bricks) == len(bricks) - 1
+
+        # n.b. It's ok to mutate other_bricks since (1) it's a copy and
+        # (2) applying gravity to `other_bricks` with `fall()` doesn't affect
+        # `bricks` because applying a gravity to one brick with
+        # `Brick.fall1_copy()` doesn't modify the bricks.
+        changed = fall(other_bricks)
+        if not changed:
+            answer += 1
+
+    print(answer)
+
+
+def fall(bricks):
+    def can_fall1(brick):
+        brick2 = brick.fall1_copy()
+        # todo-optim: Shouldn't need to loop through every brick to find a
+        # collision. Make and maintain a collision map.
+        is_ground_collision = any(
+            cube[Z] == 0
+            for cube in brick2.cubes
+        )
+        is_any_collision = is_ground_collision or any(
+            brick2.is_collision(other)
+            for other in bricks
+            if other != brick
+        )
+        return not is_any_collision
+
+    changed = False
+    while True:
+        # todo-optim: Probably sorting bricks by elevation (What does that
+        # mean? Not obvious) before looping through them would help.
+        changed_inner = False
+        for i, brick in enumerate(bricks):
+            if can_fall1(brick):
+                # todo-optim: Shouldn't need to fall by one unit repeatedly:
+                # Fall as many units as available.
+                bricks[i] = brick.fall1_copy()
+                changed_inner = True
+                changed = True
+
+        if not changed_inner:
+            return changed
+
+
+def addvec(v, w):
+    return tuple(a + b for a, b in zip(v, w))
+
+
+def subvec(v, w):
+    return tuple(a - b for a, b in zip(v, w))
+
+
+def range3d(start, end):
+    '''
+    Unlike range(), range3d includes both endpoints
+
+    >>> range3d((2, 2, 2), (2, 2, 2))
+    [(2, 2, 2)]
+    >>> range3d((0, 0, 10), (1, 0, 10))
+    [(0, 0, 10), (1, 0, 10)]
+    >>> range3d((0, 0, 1), (0, 0, 10))
+    [(0, 0, 1), (0, 0, 2), (0, 0, 3), (0, 0, 4), (0, 0, 5), (0, 0, 6), (0, 0, 7), (0, 0, 8), (0, 0, 9), (0, 0, 10)]
+    '''
+    delta = signvec(subvec(end, start))
+    result = [start]
+    pos = start
+    while pos != end:
+        pos = addvec(pos, delta)
+        result.append(pos)
+    return result
+
+
+def signvec(v):
+    return tuple(sign(a) for a in v)
+
+
+def sign(n):
+    if n > 0:
+        return 1
+    elif n < 0:
+        return -1
+    else:
+        return 0
+
+
+@dataclass
+class Brick:
+    start: ...
+    end: ...
+
+    def __post_init__(self):
+        self.cubes = set(range3d(self.start, self.end))
+
+    def is_collision(self, other):
+        return bool(self.cubes & other.cubes)
+
+    def fall1_copy(self):
+        '''
+        Fall 1 unit down. This is a copying method, not a mutating method.
+        '''
+        return Brick(
+            addvec(self.start, DOWN),
+            addvec(self.end, DOWN)
+        )
+
+
+if __name__ == '__main__':
+    main()
