f52d9deab8399685a3d29795bc1d165c51bc8206
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2023-12-25 22:09:45 -0600

    21/a.py: Various cleanup

--- 21--step-counter/a.py
+++ 21--step-counter/a.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 '''
-Usage:
-    ./a.py PATH_TO_INPUT_FILE STEP_COUNT
+Usage for puzzle input:
+    ./a.py INPUT_FILE -p
+
+Usage for example input:
+    ./a.py INPUT_FILE -e
+
+-p or -e must be provided so we know whether the Elf needs 6 more steps today
+or 64.
 '''
 
 import sys
@@ -16,14 +22,23 @@ DIRECTIONS = [
 
 
 def main():
-    world = open(sys.argv[1]).read().splitlines()
-    step_count = int(sys.argv[2])
+    # Parse args
+    if len(sys.argv) < 3 or not sys.argv[2] in ['-p', '-e']:
+        print(__doc__.strip())
+        exit()
+    path = sys.argv[1]
+    step_count = {'-p': 64, '-e': 6}[sys.argv[2]]
 
+    # Read input file
+    world = open(path).read().splitlines()
+
+    # Find start position
     for pos, ch in enumerate2d(world):
         if ch == 'S':
             start = pos
             break
 
+    # Simulate
     reachable = {pos}
     for _ in range(step_count):
         reachable = iterate(world, reachable)
